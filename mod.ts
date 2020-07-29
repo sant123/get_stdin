d837c0de6c47896ec7bcd42c10dd351cfa688a54
Santiago Aguilar H <santiago.aguilar.821@gmail.com>
2020-07-29 17:09:45 -0500

    + Added sync versions of stdin + Refactored async versions to allow reading from cli.

--- mod.ts
+++ mod.ts
@@ -1,35 +1,77 @@
-export async function getStdin() {
-  let result = "";
-  const fd = Deno.stdin;
-  const decoder = new TextDecoder();
-  let readed: number | null = null;
+const decoder = new TextDecoder();
+
+/**
+ * Returns an Uint8Array from standard input
+ */
+export async function getStdinBuffer(): Promise<Uint8Array> {
+  const bytes: number[] = [];
+
   while (true) {
-    const buffer = new Uint8Array(20);
-    readed = await fd.read(buffer);
-    if (readed === null || readed === 0) {
+    // Read bytes one by one
+    const buffer = new Uint8Array(1);
+    const readStatus = await Deno.stdin.read(buffer);
+
+    // Found EOL
+    if (readStatus === null || readStatus === 0) {
       break;
     }
-    const char = decoder.decode(buffer);
+
+    const byte = buffer[0];
+
     // On Enter, exit
-    if (char === "\n") {
+    if (byte === 10) {
       break;
     }
-    result += char;
+
+    bytes.push(byte);
   }
-  return result;
+
+  return Uint8Array.from(bytes);
 }
 
-export async function getStdinBuffer() {
-  let result;
-  const fd = Deno.stdin;
-  const buffer = new Uint8Array(20);
-  let readed: number | null = null;
+/**
+ * Returns a string from standard input
+ */
+export async function getStdin(): Promise<string> {
+  const buffer = await getStdinBuffer();
+
+  return decoder.decode(buffer);
+}
+
+/**
+ * Returns an Uint8Array from standard input in sync mode
+ */
+export function getStdinBufferSync(): Uint8Array {
+  const bytes: number[] = [];
+
   while (true) {
-    readed = await fd.read(buffer);
-    if (readed === null || readed === 0) {
+    // Read bytes one by one
+    const buffer = new Uint8Array(1);
+    const readStatus = Deno.stdin.readSync(buffer);
+
+    // Found EOL
+    if (readStatus === null || readStatus === 0) {
       break;
     }
-    result = buffer;
+
+    const byte = buffer[0];
+
+    // On Enter, exit
+    if (byte === 10) {
+      break;
+    }
+
+    bytes.push(byte);
   }
-  return result;
+
+  return Uint8Array.from(bytes);
+}
+
+/**
+ * Returns a string from standard input in sync mode
+ */
+export function getStdinSync(): string {
+  const buffer = getStdinBufferSync();
+
+  return decoder.decode(buffer);
 }
